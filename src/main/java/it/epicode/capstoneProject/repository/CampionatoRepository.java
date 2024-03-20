f9a142fffc90d3476c985a53f63c9e06cf5c2836
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-03-20 20:54:58 +0100

    Aggiunto campionato repository

---
+++ src/main/java/it/epicode/capstoneProject/repository/CampionatoRepository.java
@@ -0,0 +1,17 @@
+package it.epicode.capstoneProject.repository;
+
+import it.epicode.capstoneProject.model.entity.Campionato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CampionatoRepository extends JpaRepository<Campionato, Integer> {
+    @Query("SELECT c FROM Campionato c WHERE c.creator.username = :creatorUsername")
+    public List<Campionato> getByCreatorUsername(String creatorUsername);
+
+    @Query("SELECT c FROM Campionato c WHERE LOWER(c.nome) LIKE LOWER('%', :partialNome, '%')")
+    public List<Campionato> getByPartialNome(String partialNome);
+}
