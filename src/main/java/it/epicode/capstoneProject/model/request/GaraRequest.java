55d594e0dcbdb46545e1d6b21c83f31848041a20
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-03-20 20:51:55 +0100

    Completate request campionati

---
+++ src/main/java/it/epicode/capstoneProject/model/request/GaraRequest.java
@@ -0,0 +1,15 @@
+package it.epicode.capstoneProject.model.request;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+public class GaraRequest {
+    // NEL SERVICE VALIDARE CHE I PILOTI INSERITI APPARTENGANO AL CAMPIONATO
+    @NotNull(message = "Nome gara obbligatorio")
+    @Pattern(regexp = "^(?=.*[a-zA-ZÀ-ÿ])[a-zA-ZÀ-ÿ0-9_.,&@*%!?\\[\\]\\(\\)\\$#\\-+]{4,50}$", message = "Nome gara non valido")
+    private String nome;
+    @NotNull(message = "Gara sprint ?")
+    private Boolean sprint;
+}
