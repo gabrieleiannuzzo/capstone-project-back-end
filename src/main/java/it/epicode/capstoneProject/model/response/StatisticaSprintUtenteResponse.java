9e6020aeff0fcaad0bb16b462876122d8678a03f
gabrieleiannuzzo <gabrieleiannuzzo10@gmail.com>
2024-03-31 15:43:34 +0200

    Aggiunti metodi per creare responses

--- src/main/java/it/epicode/capstoneProject/model/response/StatisticaSprintUtenteResponse.java
+++ src/main/java/it/epicode/capstoneProject/model/response/StatisticaSprintUtenteResponse.java
@@ -1,5 +1,6 @@
 package it.epicode.capstoneProject.model.response;
 
+import it.epicode.capstoneProject.model.entity.StatisticaSprintUtente;
 import lombok.Data;
 
 @Data
@@ -10,4 +11,17 @@ public class StatisticaSprintUtenteResponse {
     private int posizionamentiTop3;
     private int numeroRitiri;
     private int numeroPenalita;
+
+    public static StatisticaSprintUtenteResponse createFromStatisticaSprintUtente(StatisticaSprintUtente s){
+        StatisticaSprintUtenteResponse response = new StatisticaSprintUtenteResponse();
+
+        response.setNumeroSprintDisputate(s.getNumeroSprintDisputate());
+        response.setPosizioneMediaGara(s.getPosizioneMediaGara());
+        response.setNumeroVittorie(s.getNumeroVittorie());
+        response.setPosizionamentiTop3(s.getPosizionamentiTop3());
+        response.setNumeroRitiri(s.getNumeroRitiri());
+        response.setNumeroPenalita(s.getNumeroPenalita());
+
+        return response;
+    }
 }
--- src/main/java/it/epicode/capstoneProject/model/response/StatisticaUtenteResponse.java
+++ src/main/java/it/epicode/capstoneProject/model/response/StatisticaUtenteResponse.java
@@ -1,5 +1,6 @@
 package it.epicode.capstoneProject.model.response;
 
+import it.epicode.capstoneProject.model.entity.StatisticaUtente;
 import lombok.Data;
 
 @Data
@@ -13,4 +14,20 @@ public class StatisticaUtenteResponse {
     private int posizionamentiTop10;
     private int numeroRitiri;
     private int numeroPenalita;
+
+    public static StatisticaUtenteResponse createFromStatisticaUtente(StatisticaUtente s){
+        StatisticaUtenteResponse response = new StatisticaUtenteResponse();
+
+        response.setNumeroGareDisputate(s.getNumeroGareDisputate());
+        response.setPosizioneMediaGara(s.getPosizioneMediaGara());
+        response.setPosizioneMediaQualifica(s.getPosizioneMediaQualifica());
+        response.setNumeroVittorie(s.getNumeroVittorie());
+        response.setNumeroPolePositions(s.getNumeroPolePositions());
+        response.setPosizionamentiTop3(s.getPosizionamentiTop3());
+        response.setPosizionamentiTop10(s.getPosizionamentiTop10());
+        response.setNumeroRitiri(s.getNumeroRitiri());
+        response.setNumeroPenalita(s.getNumeroPenalita());
+
+        return response;
+    }
 }
